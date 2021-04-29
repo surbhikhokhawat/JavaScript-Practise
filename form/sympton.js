e8d6f7996c01dcbdf77b3a5cef87a53fa1817423
Surbhi Khokhawat <you@example.com>
2021-04-30 02:52:00 +0530

    added modal for symptoms

---
+++ form/sympton.js
@@ -0,0 +1,18 @@
+var symptomData = [
+  {
+    key:"HeartDisease",
+    values:[
+      "sad",
+      "Depress" ,
+      "kjsdh"
+    ]
+  },
+  {
+    key:"BreastCancer",
+    values:[
+      "Breathing",
+      "Cough" ,
+      "kjsdh"
+    ]
+  }
+]
